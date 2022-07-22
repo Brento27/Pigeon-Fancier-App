09c27f056150dc78b7d202a95ca752a439ed4d63
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-07-22 08:44:54 +0200

    Added pagination middleware in api

--- backend/routes/pigeons.js
+++ backend/routes/pigeons.js
@@ -3,10 +3,9 @@ const Pigeon = require('../modals/pigeon');
 const router = express.Router();
 
 // Getting all
-router.get('/', async (req, res) => {
+router.get('/', paginatedResults(Pigeon), (req, res) => {
   try {
-    const pigeons = await Pigeon.find();
-    res.json(pigeons);
+    res.json(res.paginatedResults);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -91,6 +90,40 @@ async function getPigeon(req, res, next) {
 
   res.pigeon = pigeon;
   next();
+}
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (err) {
+      rest.status(500).json({ message: err.message });
+    }
+  };
 }
 
 module.exports = router;
